de03f2365c71247b3a9db42d67ee29a904b997ad
Ryan Shang <DeviLily@live.com>
2020-03-08 20:42:01 +0800

    增加+N天（收瓦特/不收瓦特）、捕捉并喂糖三种模式的入口。

--- arduino_firmware/arduino_firmware.ino
+++ arduino_firmware/arduino_firmware.ino
@@ -115,15 +115,21 @@ void loop() {
      * 4. Entry of +3
      * 5. Entry of R+1 S+3
      * 6. Entry of R+3/4/5
-     * 7. Entry of Capture and Levelup  TODO
+     * 7. Entry of Capture and Levelup
+     * 8. Entry of +N with watt
+     * 9. Entry of +N without watt
      * 10. R+3/4/5 - Input days
      * 11. +3 - Confirm
      * 12. R+1 S+3 - Confirm
+     * 13. Capture and Levelup - Confirm
+     * 14. +N with watt - Input days
+     * 15. +N without watt - Input days
      */
-    if (refresh) {
+
+    if (refresh) {  // refresh display
       refresh = 0;
       switch (mode) {
-      case 1:
+      case 1: // yy:yy
         DispNDigit(3, 4, year);
         break;
 
@@ -154,6 +160,26 @@ void loop() {
         num_disp[2] = 0x0A;
         break;
       
+      case 7: // 4_:CA
+        num_disp[0] = 4;
+        num_disp[1] = BLANK;
+        num_disp[2] = 0x0C;
+        num_disp[3] = 0x0A;
+        break;
+      
+      case 8: // 5_:ny
+        num_disp[0] = 5;
+        num_disp[1] = BLANK;
+        num_disp[2] = CHAR_N;
+        num_disp[3] = CHAR_Y;
+        break;
+      
+      case 9: // 6_:nn
+        num_disp[0] = 6;
+        num_disp[1] = BLANK;
+        num_disp[2] = num_disp[3] = CHAR_N;
+        break;
+      
       case 10:// R_:0d
         DispNDigit(3, 2, day_cnt);
         num_disp[0] = 0x0A;
@@ -161,11 +187,17 @@ void loop() {
       
       case 11:// _y:n_
       case 12:
+      case 13:
         num_disp[1] = num_disp[2] = BLANK;
         num_disp[0] = CHAR_Y;
         num_disp[3] = CHAR_N;
         break;
       
+      case 14:// dd:dd
+      case 15:
+        // maintain
+        break;
+
       default:
         DispNDigit(3, 0, 1);  // Clear Display
         break;
@@ -173,174 +205,216 @@ void loop() {
       tm1637.point(1);
       tm1637.display(num_disp);
     }
-    if (btn_stat & 1) {
+
+    if (btn_stat & 1) { // key 1 as up
       btn_stat &= (~1);
+      refresh = 1;
       switch (mode) {
       case 1:
         ++year;
-        refresh = 1;
         break;
       
       case 2:
         ++month;
         if (month > 12) month = 1;
-        refresh = 1;
         break;
       
       case 3:
         ++day;
         if (day > month_day[month] + (month == 2 ? is_leap_y : 0)) day = 1;
-        refresh = 1;
         break;
       
       case 4:
-        // TODO to last mode entry;
-        refresh = 1;
+        mode = 9;
         break;
       
       case 5:
       case 6:
       case 7:
+      case 8:
+      case 9:
         --mode;
-        refresh = 1;
         break;
 
       case 10:
         ++day_cnt;
         if (day_cnt > 5) day_cnt = 3;
-        refresh = 1;
         break;
-
-      default:  // 11, 12
+      
+      case 14:
+      case 15:
+        ++num_disp[cur_pos];
+        if (num_disp[cur_pos] > 9) num_disp[cur_pos] = 0;
+        break;
+      
+      default:  // 11, 12, 13
         break;
       }
-    } else if (btn_stat & 2) {
+    } else if (btn_stat & 2) {  // key 2 as down (or right)
       btn_stat &= (~2);
+      refresh = 1;
       switch (mode) {
       case 1:
         --year;
-        refresh = 1;
         break;
       
       case 2:
         --month;
         if (month < 1) month = 12;
-        refresh = 1;
         break;
       
       case 3:
         --day;
         if (day < 1) day = month_day[month] + (month == 2 ? is_leap_y : 0);
-        refresh = 1;
         break;
       
       case 4:
       case 5:
       case 6:
+      case 7:
+      case 8:
         ++mode;
-        refresh = 1;
         break;
       
-      case 7:
-        // TODO to first mode entry
+      case 9:
         mode = 4;
-        refresh = 1;
         break;
 
       case 10:
         --day_cnt;
         if (day_cnt < 3) day_cnt = 5;
-        refresh = 1;
+        break;
+      
+      case 14:
+      case 15:
+        ++cur_pos;
+        if (cur_pos > 3) cur_pos = 0;
         break;
 
-      default:  // 11, 12
+      default:  // 11, 12, 13
         break;
       }
-    } else if (btn_stat & 4) {
+    } else if (btn_stat & 4) {  // key 3 as B (cancel)
       btn_stat &= (~4);
+      refresh = 1;
       switch (mode) {
       case 2:
         mode = 1;
-        refresh = 1;
         break;
       
       case 3:
         day = 1;
         mode = 2;
-        refresh = 1;
         break;
       
       case 10:
         mode = 6;
-        refresh = 1;
         break;
       
       case 11:
         mode = 4;
-        refresh = 1;
         break;
       
       case 12:
         mode = 5;
-        refresh = 1;
+        break;
+      
+      case 13:
+        mode = 7;
+        break;
+      
+      case 14:
+        mode = 8;
+        break;
+      
+      case 15:
+        mode = 9;
         break;
 
-      default:  // 1, 4, 5, 6, 7
+      default:  // 1, 4, 5, 6, 7, 8, 9
         break;
       }
-    } else if (btn_stat & 8) {
+    } else if (btn_stat & 8) {  // key 4 as A (confirm)
       btn_stat &= (~8);
+      refresh = 1;
       switch (mode) {
       case 1:
         init_year = year;
         is_leap_y = IsLeapYear();
         mode = 2;
-        refresh = 1;
         break;
 
       case 2:
         init_month = month;
         mode = 3;
-        refresh = 1;
         break;
       
       case 3:
         init_day = day;
         mode = 4;
-        refresh = 1;
         break;
       
       case 4:
         day_cnt = 3;
         mode = 11;
-        refresh = 1;
         break;
       
       case 5:
         day_cnt = 1;
         mode = 12;
-        refresh = 1;
         break;
 
       case 6:
         day_cnt = 3;
         mode = 10;
-        refresh = 1;
+        break;
+      
+      case 7:
+        mode = 13;
+        break;
+      
+      case 8:
+        DispNDigit(3, 4, 0);
+        cur_pos = 0;
+        mode = 14;
+        break;
+      
+      case 9:
+        DispNDigit(3, 4, 0);
+        cur_pos = 0;
+        mode = 15;
         break;
       
       case 10:
         mode = 20;
-        refresh = 1;
         break;
       
       case 11:
         mode = 22;
-        refresh = 1;
         break;
       
       case 12:
         mode = 30;
-        refresh = 1;
+        break;
+      
+      case 13:
+        mode = 70;
+        break;
+      
+      case 14:
+        day_cnt = 0;
+        for (i = 0; i < 4; ++i) {
+          day_cnt = day_cnt * 10 + num_disp[i];
+        }
+        mode = 50;
+        break;
+      
+      case 15:
+        day_cnt = 0;
+        for (i = 0; i < 4; ++i) {
+          day_cnt = day_cnt * 10 + num_disp[i];
+        }
+        mode = 60;
         break;
 
       default:
@@ -367,6 +441,25 @@ void loop() {
      * 39. Back to Now
      * 40. Date to Game
      * 41. Save
+     * 
+     * 50 - 59: +N with watt        // TODO
+     * 50. 领瓦特，并返回
+     * 51. Game to Date
+     * 52. Date Plus One
+     * 53. Date to Game
+     * 
+     * 60 - 69: +N without watt     // TODO
+     * 60. Game to Date
+     * 61. Date Plus One
+     * 62. Date Plus N
+     * 63. Date to Game
+     * 64. Save
+     * 
+     * 70 - 79: Capture and Levelup // TODO
+     * 70. 捕捉，返回游戏界面
+     * 71. 打开背包，喂糖
+     * 72. 取消学习技能（取消进化）
+     * 73. 退出背包，查看宝可梦信息
      * 
      */
     if (refresh) {
@@ -510,6 +603,11 @@ void loop() {
       day_cnt = 3;
       refresh = 1;
       mode = 22;
+      break;
+    
+    default:
+      mode = 6;
+      refresh = 1;
       break;
     }
 
